07787cc8f8e1d5e3220c2562c9105154ff439ff6
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-26 14:42:27 +0300

    first commit

---
+++ first.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome("C:\\Users\\karak\\OneDrive\\Masaüstü\\chromedriver")
+
+browser.get("http://yaz.tf.firat.edu.tr/")
+time.sleep(3)
